7509666e6b27ee7573bbba57270644d876c395b7
Jhonatan Jauja <jhonatan.jauja.c@uni.pe>
2020-05-23 21:09:03 -0500

    Se agrego la grafica de una funcion coseno

--- prueba_git.py
+++ prueba_git.py
@@ -1,2 +1,13 @@
 print("Hola mundo, estoy aprendiendo a usar git.")
 print("ESTE ES EL PRIMER CAMBIO PARA ESTE CODIGO :D")
+
+import numpy as np 
+import matplotlib.pyplot as plt
+
+x=np.linspace(-5,5,1000)
+def f(x):
+    return np.cos(x)
+plt.plot(x,f(x),"r",label="Funcion coseno")
+plt.legend(loc=2)
+plt.grid()
+plt.show()
